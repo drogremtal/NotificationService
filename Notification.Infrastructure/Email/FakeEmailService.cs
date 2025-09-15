02e1694fd2bbe3d818ad9b0637f41a9a343a69ff
drogremtal <bp240@outlook.com>
2025-09-15 20:25:13 +0300

    +

---
+++ Notification.Infrastructure/Email/FakeEmailService.cs
@@ -0,0 +1,30 @@
+﻿using NotificationService.Application.Dtos;
+using NotificationService.Application.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotificationService.Infrastructure.Email
+{
+    public class FakeEmailService : ISmtpEmailService
+    {
+        public async Task<bool> SendMessage(EmailNotification emailNotification)
+        {
+
+            var notification = @$"
+
+                {emailNotification.IsHtml}
+                {emailNotification.Recipient}
+                {emailNotification.ReplyTo}
+                {emailNotification.Subject}
+                {emailNotification.Body}";
+
+            Console.WriteLine(notification);
+            return true;
+        }
+
+
+    }
+}
